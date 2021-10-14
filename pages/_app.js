de4b7d215002f9d9736d29e443a2c6227add0104
forecho <caizhenghai@gmail.com>
2021-10-14 09:49:24 +0800

    fix: fix bug

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,8 @@
 import "../styles/globals.css";
 import "tailwindcss/tailwind.css";
 import { ThemeProvider } from "next-themes";
-import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      ga.pageview(url);
-    };
-    //When the component is mounted, subscribe to router changes
-    //and log those page views
-    router.events.on("routeChangeComplete", handleRouteChange);
-
-    // If the component is unmounted, unsubscribe
-    // from the event with the `off` method
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
-
   return (
     <ThemeProvider defaultTheme="light" attribute="class">
       <Component {...pageProps} />
